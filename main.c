04991b74b79a64e10ec17fddda6674c6f191dd53
Darius Arnold <mail@dariusarnold.de>
2025-01-30 22:40:20 +0100

    Refactor
    - Use a common notation for structs and struct member access
    - Introduce specific return types instead of reusing one
    - Add whitespace in comments and around binary operators

--- main.c
+++ main.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 
-struct Result{
-    double value1;
-    double value2;
+struct ResultVinc {
+    double distance;
+    double azimuth;
 };
 
-typedef struct geom {
-    double sin_sigma, cos_sigma, sigma, sin_alpha, cos_sq_alpha, cos2sigma;
-} geom; 
+struct Geom {
+    double sin_sigma;
+    double cos_sigma;
+    double sigma;
+    double sin_alpha;
+    double cos_sq_alpha;
+    double cos2sigma;
+};
 
-void calcgeo(geom *g, double lam, double u1, double u2) {
-
-    (*g).sin_sigma = sqrt(pow((cos(u2) * sin(lam)), 2.) + pow(cos(u1)*sin(u2) - sin(u1)*cos(u2)*cos(lam), 2.));
-    (*g).cos_sigma = sin(u1) * sin(u2) + cos(u1) * cos(u2) * cos(lam);
-    (*g).sigma = atan((*g).sin_sigma / (*g).cos_sigma);
-    if ((*g).sigma <= 0) (*g).sigma = M_PI + (*g).sigma;
-    (*g).sin_alpha = (cos(u1) * cos(u2) * sin(lam)) / (*g).sin_sigma;
-    (*g).cos_sq_alpha = 1 - pow((*g).sin_alpha, 2.);
-    (*g).cos2sigma = (*g).cos_sigma - ((2 * sin(u1) * sin(u2)) / (*g).cos_sq_alpha);
-
+void calcgeo(struct Geom *g, double lam, double u1, double u2) {
+    g->sin_sigma = sqrt(pow((cos(u2) * sin(lam)), 2.) + pow(cos(u1) * sin(u2) - sin(u1) * cos(u2) * cos(lam), 2.));
+    g->cos_sigma = sin(u1) * sin(u2) + cos(u1) * cos(u2) * cos(lam);
+    g->sigma = atan(g->sin_sigma / g->cos_sigma);
+    if (g->sigma <= 0) {
+        g->sigma += M_PI;
+    }
+    g->sin_alpha = (cos(u1) * cos(u2) * sin(lam)) / g->sin_sigma;
+    g->cos_sq_alpha = 1 - pow(g->sin_alpha, 2.);
+    g->cos2sigma = g->cos_sigma - ((2 * sin(u1) * sin(u2)) / g->cos_sq_alpha);
 };
 
 /**
@@ -32,12 +37,10 @@ void calcgeo(geom *g, double lam, double u1, double u2) {
  * @param longc Longitude
  * @return
  */
-struct Result vinc(double latp, double latc, double longp, double longc) {
-
-    geom gvar;
-
-    double req = 6378137.0;             //Radius at equator
-    double flat = 1 / 298.257223563;    //flattenig of earth
+struct ResultVinc vinc(double latp, double latc, double longp, double longc) {
+    struct Geom gvar;
+    double req = 6378137.0;             // Radius at equator
+    double flat = 1 / 298.257223563;    // Flattening of earth
     double rpol = (1 - flat) * req;
 
     double u1, u2, lon, lam, tol, diff;
@@ -54,7 +57,7 @@ struct Result vinc(double latp, double latc, double longp, double longc) {
 
     lon = longp - longc;
     lam = lon;
-    tol = pow(10., -12.); // iteration tolerance
+    tol = pow(10., -12.); // Iteration tolerance, 10E-12 corresponds to approx. 0.06 mm
     diff = 1.;
 
     while (fabs(diff) > tol) {
@@ -76,9 +79,14 @@ struct Result vinc(double latp, double latc, double longp, double longc) {
     dis = rpol * A * (gvar.sigma - delta_sig);
     azi1 = atan2((cos(u2) * sin(lam)), (cos(u1) * sin(u2) - sin(u1) * cos(u2) * cos(lam)));
 
-    struct Result res = {dis, azi1};
-    return(res);
+    struct ResultVinc r = {dis, azi1};
+    return r;
 }
+
+struct ResultTrans {
+    double x;
+    double y;
+};
 
 /**
  * Transform longitude, latitude coordinates of two points to x,y coordinates
@@ -88,29 +96,25 @@ struct Result vinc(double latp, double latc, double longp, double longc) {
  * @param longc
  * @return
  */
-struct Result trans(double latp, double latc, double longp, double longc){
+struct ResultTrans trans(double latp, double latc, double longp, double longc){
 
-    double rav, theta, dis, azi, xy, x, y;
-    struct Result dis_azi;
+    double rav, theta, xy, x, y;
+    struct ResultVinc dis_azi;
 
-    rav = 6371000.0;  //average radius
+    rav = 6371000.0;  // average radius
     dis_azi = vinc(latp,latc,longp,longc);
 
-    dis = dis_azi.value1;
-    azi = dis_azi.value2;
-
-    theta = dis/rav; //finding theta angle
-    xy = sin(theta)*rav; //length in xy plane
-
-    y = xy*cos(azi); //lat for chunk
-    x = xy*sin(azi); //long for chunk
-    struct Result res = {y, x};
-    return(res);
+    theta = dis_azi.distance / rav; // finding theta angle
+    xy = sin(theta) * rav; // length in xy plane
+    y = xy * cos(dis_azi.distance); // lat for chunk
+    x = xy * sin(dis_azi.azimuth); // long for chunk
+    struct ResultTrans r = {x, y};
+    return r;
 }
 
 int main() {
     // Testing with one value
-    struct Result res = trans(40.99698, 46.0, 9.20127, 10.0);
-    printf("%f - %f\n", res.value1, res.value2);
+    struct ResultTrans res = trans(40.99698, 46.0, 9.20127, 10.0);
+    printf("%f - %f\n", res.x, res.y);
     return 0;
 }
